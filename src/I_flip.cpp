faa41d7bf6a1f07037bdce0b938d48266410559f
Smehta <mehta.sahil1997@gmail.com>
2018-04-09 14:48:26 +0200

    I_flip, I_mark added

--- src/I_flip.cpp
+++ src/I_flip.cpp
@@ -1,10 +1,3 @@
-/*
- * I_flip Implementation
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -19,30 +12,21 @@ void I_flip::execute(Bug& b) {
     int randomI =randomint();
     if(randomI == 0)
         b.set_state(x);
-    else 
+    else
         b.set_state(y);
 }
 
-void I_flip::parse(std::string& args){
-    std::vector<std::string> v;
+void I_flip::parse(std::string args){
+  std::vector<std::string> v;
+  char* token = strtok((char*)args.c_str(), " ,.-");
 
-    char * token;
+  while (token != NULL){
+  	std::string str(token);
+  	v.push_back(str);
+    token = strtok (NULL, " ,.-");
+  }
 
-    token = strtok ( (char*) args.c_str()," ,.-");
-    while (token!= NULL)
-    {
-        std::string str(token);
-        v.push_back(str);
-        token= strtok (NULL, " ,.-");
-    }
-    if(v[0] != "flip")
-        throw std::runtime_error("Wrong instruction called!");
-	int p = std::stoi(v[1],nullptr,10);
-	this->x.val = std::stoi(v[1],nullptr,10);
-	this->y.val = std::stoi(v[1],nullptr,10);
-}
-
-int I_flip::randomint() {
-    srand(time(NULL));
-    return rand() % (this->p - 1);
+	int p = std::stoi(v[0], nullptr, 10);
+	this->x.val = std::stoi(v[1], nullptr, 10);
+	this->y.val = std::stoi(v[2], nullptr, 10);
 }
--- src/I_mark.cpp
+++ src/I_mark.cpp
@@ -9,35 +9,30 @@
 #include <exception>
 #include <vector>
 #include <cstring>
-
+#include "attribute.h"
 #include "I_mark.h"
 #include "World.h"
 
 void I_mark::execute(Bug& b){
     //set mark m and go to state z
-    position_t p = b.get_position();
+    tposition p = b.get_position();
     Cell *x = this->w.get_cell(p);
-    mark_t newmark;
-    newmark.val = m;
-    x->mark.set_marker(newmark, b.get_color());
+    tmark new_mark;
+    new_mark.val = m;
+    x->mark.set_marker(new_mark, b.get_color());
     b.set_state(z);
 }
 
 void I_mark::parse(std::string& args){
     std::vector<std::string> v;
-
-    char * token;
+    char* token;
 
     token = strtok ( (char*) args.c_str()," ,.-");
-    while (token!= NULL)
-    {
+    while (token!= NULL){
         std::string str(token);
         v.push_back(str);
         token= strtok (NULL, " ,.-");
     }
-    if(v[0] != "mark")
-        throw std::runtime_error("Wrong instruction called!");
-    int m= std::stoi (v[1],nullptr,10);
-    this->z.val = std::stoi (v[2],nullptr,10);
-    
+    int m= std::stoi (v[0], nullptr, 10);
+    this->z.val = std::stoi (v[1], nullptr, 10);
 }
